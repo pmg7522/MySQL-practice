af4a462227e4e8d2c5a797e541ba2190146833d9
pmg7522 <pmg752290@gmail.com>
2022-07-15 10:36:31 +0900

    회원가입 작업중

--- controllers/user/signup.js
+++ controllers/user/signup.js
@@ -2,10 +2,11 @@ import { db } from "./config/db.js";
 import crypto from "crypto-js"
 
 const { username, password } = req.body;
-const params = [username, password];
 
 const hash = crypto.SHA256(adminPassword, process.env.SALT).toString();
+
 const sql = 'INSERT INTO User(`username`, `password`), VALUES(?, ?,)';
+const params = [username, password];
 
 db.query(sql, params, (err, row) => {
   if (err) {
