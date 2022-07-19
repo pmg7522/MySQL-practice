baff0c973fcab3e26cb3bb7f5074bd96aeefd866
pmg7522 <pmg752290@gmail.com>
2022-07-19 09:18:18 +0900

    유저 회원가입 코드 수정

--- controllers/user/signup.js
+++ controllers/user/signup.js
@@ -3,9 +3,9 @@ import crypto from "crypto-js"
 
 const { username, password } = req.body;
 
-const hash = crypto.SHA256(adminPassword, process.env.SALT).toString();
+const hash = crypto.SHA256(password, process.env.SALT).toString();
 
-const sql = 'INSERT INTO User(`username`, `password`), VALUES(?, ?,)';
+const sql = 'INSERT INTO User(`username`, `password`) VALUES(?, ?)';
 const params = [username, password];
 
 db.query(sql, params, (err, row) => {
