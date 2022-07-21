abee6a043f6d328bd12635b74b71ba56206947fd
pmg7522 <pmg752290@gmail.com>
2022-07-21 12:02:38 +0900

    router 작업

--- controllers/user/signup.js
+++ controllers/user/signup.js
@@ -1,15 +1,17 @@
 import { db } from "./config/db.js";
 import crypto from "crypto-js"
 
-const { username, password } = req.body;
+export default = (req, res) => {
+  const { username, password } = req.body;
 
-const hash = crypto.SHA256(password, process.env.SALT).toString();
+  const hash = crypto.SHA256(password, process.env.SALT).toString();
 
-const sql = 'INSERT INTO User(`username`, `password`) VALUES(?, ?)';
-const params = [username, password];
+  const sql = 'INSERT INTO User(`username`, `password`) VALUES(?, ?)';
+  const params = [username, password];
 
-db.query(sql, params, (err, row) => {
-  if (err) {
-    console.log(err);
-  }
-});
+  db.query(sql, params, (err, row) => {
+    if (err) {
+      console.log(err);
+    }
+  });
+}
