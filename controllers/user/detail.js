8d9ae70f56b8f7eb815f14c51fb14b9160b754c8
pmg7522 <pmg752290@gmail.com>
2022-08-02 09:06:14 +0900

    유저 상세정보 요청 완료

--- controllers/user/detail.js
+++ controllers/user/detail.js
@@ -2,7 +2,10 @@ const User = require('../../models/user');
 
 module.exports = async (req, res, next) => {
   const { id } = req.params;
-  console.log(1)
+
   const userData = await User.getUserDetail(id);
+
+  return res.status(200).send({
+    message: "유저 정보 요청이 완료되었습니다."
+  })
 };
-  // console.log(4, await userData());
